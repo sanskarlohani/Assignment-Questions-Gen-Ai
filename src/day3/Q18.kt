bfab0b6c00d4f8f62a096ee283816292189910a3
sanskar <sanskarlohani730@gmail.com>
2025-06-06 23:16:11 +0530

    Q18 Done

--- src/day3/Q18.kt
+++ src/day3/Q18.kt
@@ -4,8 +4,8 @@ fun main () {
     val tipNumber = (1..4).random()
     when(tipNumber) {
         1 -> println("Tip: Always stir your coffee!")
-        2 -> println("Tip: Try it black once in a while!")
-        3 -> println("Tip: Don't forget to smile while sipping!")
-        4 -> println("Tip: Double shot means double energy!")
+        2 -> println("Tip: Try Black Coffee SomeDay")
+        3 -> println("Tip: Don't forget to give a Tip ")
+        4 -> println("Tip: double energy drink coffee")
     }
 }
